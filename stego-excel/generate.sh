6a7579c1bd2f324824595afe7e540e924431e70a
Alexander Korzun <korzun.sas@mail.ru>
2019-07-18 16:42:13 +0300

    Add stego-excel working and generated task

---
+++ stego-excel/generate.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! which bsdtar &>/dev/null; then
+    echo 'Error: bsdtar is not installed, but it is required to generate the task'
+    exit 1
+fi
+
+if ! which python3 &>/dev/null; then
+    echo 'Error: python3 is not installed, but it is required to generate the task'
+    exit 1
+fi
+
+FLAG='LKLCTF{3XceL_F1Le_i5_n0t_0n1Y_a_T4blE_2be0915a_4810eac0}'
+
+shopt -s dotglob
+set -x
+
+python3 ./gen-helper.py
+
+rm -rf 'tmp'
+mkdir -p 'tmp'
+cd 'tmp'
+bsdtar -xf ../Transactions-info.xlsx
+printf "<!-- %s -->\n" "${FLAG}" >> 'xl/styles.xml'
+bsdtar -cavf ../Transactions-info.zip *
+cd ..
+mv Transactions-info.zip Transactions-info.xlsx
