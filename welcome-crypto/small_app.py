993026b2944c7c8b282a092cd95569b958002a96
kochan <kochanac@ya.ru>
2019-07-25 15:29:00 +0300

    docker

--- welcome-crypto/small_app.py
+++ welcome-crypto/small_app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask import render_template_string, request
 app = Flask(__name__)
-flag = 'LKLCTF{придумайте флаг пж ребятаа}'
+flag = 'LKLCTF{1s_7h1s_ha$h_c0lI1$ion}'
 
 def hash(text):
-	prime = 53
-	s = 0
-	for i in range(len(text)):
-		s += prime**i*ord(text[i])
+    prime = 53
+    s = 0
+    for i in range(len(text)):
+        s += prime**i*ord(text[i])
 
-	return s
+    return s
 
 template = """
 <form><input name="password"/><button type="submit">log in</button></form>
@@ -19,16 +19,16 @@ template = """
 
 @app.route('/', methods=['GET'])
 def index():
-	passw = request.args.get('password', 'lox')
-	print(passw, hash(passw))
-	if passw == 'lox':
-		return render_template_string(template)
-	else:
-		if hash(passw) == hash("3=1`;zqE#F!245C"):
-			return render_template_string(template, flag=flag)
-		else:
-			return render_template_string(template, flag='wrong pass')
+    passw = request.args.get('password', 'lox')
+    print(passw, hash(passw))
+    if passw == 'lox':
+        return render_template_string(template)
+    else:
+        if hash(passw) == hash("3=1`;zqE#F!245C"):
+            return render_template_string(template, flag=flag)
+        else:
+            return render_template_string(template, flag='wrong pass')
 
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=802)
+    app.run(host="0.0.0.0", port=802)
