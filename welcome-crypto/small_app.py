bba57e8f73bba249ef78225f4341ea1b4e5a5a3e
Maxim <supermax74.02@gmail.com>
2019-08-13 18:02:13 +0500

    Small adjustments and fixes

--- welcome-crypto/small_app.py
+++ welcome-crypto/small_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template_string, request, session, redirect
+import secrets
+
 app = Flask(__name__)
 flag = 'LKLCTF{1s_7h1s_ha$h_c0lI1$ion}'
 SALT = "Это нацист, это нацист, это нацист"
-app.config["SECRET_KEY"] = b"enis"
+app.config["SECRET_KEY"] = secrets.token_bytes(10)
 
 
 def hash(text):
@@ -11,22 +13,23 @@ def hash(text):
     p = 17
     s = 0
     for i in range(len(text)):
-        s += p**i*ord(text[i])
+        s += p ** i * ord(text[i])
 
     return s
 
+
 template = """
 <form>
-    <input name="text" placeholder="paycheck here"/>
-    <input name="hash" placeholder="hash here"/>
-    <button type="submit">log in</button>
+    <input name="text" placeholder="Paycheck"/>
+    <input name="hash" placeholder="Hash"/>
+    <button type="submit">Redeem</button>
 </form>
 
 <h3>{{msg}}</h3>
 
-your balance is <h2>{{session.money}}</h2>
+Your balance is <h2>{{session.money}}</h2>
 <form action="/reset">
-    <button type="submit">reset money</buton>
+    <button type="submit">Reset money</buton>
 </form>
 
 """
@@ -41,11 +44,11 @@ def index():
         session["money"] = 0
 
     if not (H and paycheck):
-        return render_template_string(template, )
+        return render_template_string(template)
     else:
         if hash(paycheck) == int(H) and paycheck.isprintable():
             if session["money"] != 0:
-                return render_template_string(template, msg="you cant activate more than one paycheck")
+                return render_template_string(template, msg="You cant activate more than one paycheck. Please reset.")
             else:
                 session["money"] += int(paycheck.split()[-1])
                 if session["money"] >= 1000:
@@ -54,14 +57,13 @@ def index():
                     return render_template_string(template)
         else:
             print(H, paycheck, hash(paycheck))
-            return render_template_string(template, msg='bad paycheck: not-printable symbols or incorrect hash')
+            return render_template_string(template, msg='Bad paycheck: not-printable symbols or incorrect signature')
+
 
 @app.route('/reset')
 def reset():
     session["money"] = 0
     return redirect('/')
-
-
 
 # if __name__ == '__main__':
 #     app.config["SECRET_KEY"] = b"enis"
